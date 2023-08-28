a560f431e7926bdc4a00f134f5f5c88f803b6663
kdrbdi <badaoui.abk@gmail.com>
2023-08-28 17:59:32 +0100

    Add functionality to generate a custom grid Add hover effect to imitate the "etch-a-sketch" effect

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector("#container");
+const gridBtn = document.querySelector("#generate-grid");
+
+gridBtn.addEventListener("click", generateGrid);
+
+function generateGrid(numCells = 16) {
+  container.innerHTML = "";
+  do {
+    numCells = parseInt(prompt("num? max 100"));
+  } while (!numCells || numCells > 100);
+
+  for (let row = 0; row < numCells; row++) {
+    const rowBreak = document.createElement("div");
+    rowBreak.classList.add("break");
+    container.appendChild(rowBreak);
+    for (let cell = 0; cell < numCells; cell++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      container.appendChild(square);
+    }
+  }
+}
+
+const cells = document.querySelectorAll(".square");
+
+cells.forEach((cell) => {
+  cell.addEventListener("mouseover", function (e) {
+    e.target.classList.add("hovered");
+  });
+});
